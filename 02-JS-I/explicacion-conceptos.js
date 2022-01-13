96ab66ef9907a25ee2c22e8742f3964a44801a22
paixa <paixa.s92@gmail.com>
2022-01-12 21:04:13 -0300

    archivo agregado, explicacion-conceptos.js

---
+++ 02-JS-I/explicacion-conceptos.js
@@ -0,0 +1,20 @@
+//una variable, es una forma de almacenar en memoria un dato especifico//
+//la forma de crearla en javascript es mediante la palabra "var" + un espcacio + nombre //
+// ejemplo :  var nombre_de_variable//
+
+// los strings son bloques de texto, en JS siempre se definen entre comillas, dobles o simples//
+
+// las funcuiones son objetos invocables (callables objects), se utilizan para manipular variables//
+//se definen mediante la palabra var seguido del nombre de la variable, luegos los parentesis cercanos y finalmente las llaves//
+//en los parentesis cercanos se introducen los argumentos de la funcion y en las llaves todo el codigo que utiliza la funcion//
+
+// los parametros de la funcion son datos externos a la misma que se introducen cuando es necesario que la funcion interactue con ellos//
+//se pueden agragar multiples argumentos a una variable utilizando una coma para separarlos//
+
+// la declaracion de "return" se ultiza para devolver algo desde adentro de una funcion, es decir es la forma mediante la cual los datos "escapan" de la funcion
+
+// la declaracion "IF" es una forma de control de flujo de nuestro codigo, es decir una declaracion que verifica si una funcion arroja un resultado (true)//
+// para luego de esa forma ejecutar una parte especifica o bien en el caso de ser (false), continuar ejecutando el programa en caso de que no se cumpla.//
+
+//los valores booleanos "true" o "false" provienen de la logica de boole y son estructuras matematicas para esquematizar operaciones logicas en JS se usan para significar verdadero o falso//
+
